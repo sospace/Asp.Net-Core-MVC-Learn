96980794e9a17841765199a138fe611525fe471e
SuperObject <57763558@qq.com>
2019-02-28 10:57:17 +0800

    增加了一个查看用户的页面

---
+++ Asp.Net-Core-MVC-Learn/Asp.Net-Core-MVC-Learn/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_MVC_Learn.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Users Mange.";
+            return View();
+        }
+    }
+}
