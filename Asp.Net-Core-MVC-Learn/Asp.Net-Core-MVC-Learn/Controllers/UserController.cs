d48fe55d91b7c3ae4b34da94099a372e79875ea2
SuperObject <57763558@qq.com>
2019-03-11 15:22:05 +0800

    学习简单文件上传

--- Asp.Net-Core-MVC-Learn/Asp.Net-Core-MVC-Learn/Controllers/UserController.cs
+++ Asp.Net-Core-MVC-Learn/Asp.Net-Core-MVC-Learn/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.Net_Core_MVC_Learn.Controllers
@@ -39,6 +41,31 @@ namespace Asp.Net_Core_MVC_Learn.Controllers
                 return HtmlEncoder.Default.Encode(text);
             else
                 return text;
+        }
+
+        //文件上传
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath });
         }
     }
 }
